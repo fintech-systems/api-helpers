581d10770e7c5fba10bf64b2583d388f806866fa
eugenevdm <eugenevdm@users.noreply.github.com>
2021-10-15 18:15:24 +0000

    Fix styling

--- src/Api.php
+++ src/Api.php
@@ -24,7 +24,7 @@ class Api
         ]);
 
         $response = curl_exec($curl);
-        
+
         curl_close($curl);
 
         return $response;
@@ -35,16 +35,16 @@ class Api
         $curl = curl_init();
 
         curl_setopt_array($curl, [
-            CURLOPT_URL            => $url,
+            CURLOPT_URL => $url,
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING       => '',
-            CURLOPT_MAXREDIRS      => 10,
-            CURLOPT_TIMEOUT        => 0,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
             CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST  => 'POST',
-            CURLOPT_POSTFIELDS     => $postFields,
-            CURLOPT_HTTPHEADER     => $header,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $postFields,
+            CURLOPT_HTTPHEADER => $header,
         ]);
 
         $response = curl_exec($curl);
--- src/Commands/LaravelApiHelpersCommand.php
+++ src/Commands/LaravelApiHelpersCommand.php
@@ -5,13 +5,12 @@ namespace FintechSystems\LaravelApiHelpers\Commands;
 use Illuminate\Console\Command;
 
 class LaravelApiHelpersCommand extends Command
-{    
+{
     protected $name = 'placeholder'; // Avoid The command defined in "..." cannot have an empty name.
 
     public function checkCachedFileExists()
-    {        
-        if ($this->option('cached')) {            
-
+    {
+        if ($this->option('cached')) {
             if (! file_exists($this->cachedFile)) {
                 $this->error("--cached was specified but the file $this->cachedFile does not exist");
 
