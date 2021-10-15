afb2e956242eaca0182b8b43306e1219f78d2a71
eugenevdm <eugenevdm@users.noreply.github.com>
2021-10-15 22:27:16 +0000

    Fix styling

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,8 +6,8 @@ it('can test', function () {
     expect(true)->toBeTrue();
 });
 
-it('can do http get', function() {
-    $api = new Api;
+it('can do http get', function () {
+    $api = new Api();
     $result = json_decode($api->get("https://v2.jokeapi.dev/joke/Any"));
     expect($result->error)->toBeFalse();
 });
